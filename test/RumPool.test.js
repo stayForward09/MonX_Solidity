fd5f7ef1b37298b763eb870d6950e47c8739a7aa
AnichoKraus <anichokraus3123@gmail.com>
2021-04-05 13:39:46 -0400

    convert unitests to hardhat - 1

--- test/RumPool.test.js
+++ test/RumPool.test.js
@@ -1,8 +1,7 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 const { expectRevert, time } = require('@openzeppelin/test-helpers');
-const Monoswap = artifacts.require('Monoswap');
-const MockERC20 = artifacts.require('MockERC20');
-const vUSD = artifacts.require('VUSD');
-const MonoswapToken = artifacts.require('MonoswapToken');
+
 
 const e18 = 1 + '0'.repeat(18)
 const e26 = 1 + '0'.repeat(26)
@@ -11,209 +10,223 @@ const e24 = 1 + '0'.repeat(24)
 const bigNum = num=>(num + '0'.repeat(18))
 const smallNum = num=>(parseInt(num)/bigNum(1))
 
-contract('OptionVaultPair', ([alice, bob, minter, dev]) => {
-    beforeEach(async () => {
-        this.weth = await MockERC20.new('WETH', 'WETH', e26, {from: minter});
-        this.yfi = await MockERC20.new('YFI', 'YFI', e26, {from: minter});
-        this.dai = await MockERC20.new('Dai', 'DAI', e26, {from: minter});
-        this.vusd = await vUSD.new({from: minter});
+describe('OptionVaultPair', function () {
+    before(async function () {
+        this.signers = await ethers.getSigners()
+        this.alice = this.signers[0]
+        this.bob = this.signers[1]
+        this.carol = this.signers[2]
+        this.dev = this.signers[3]
+        this.minter = this.signers[4]
+        this.Monoswap = await ethers.getContractFactory('Monoswap');
+        this.MockERC20 = await ethers.getContractFactory('MockERC20');
+        this.vUSD = await ethers.getContractFactory('VUSD');
+        this.MonoswapToken = await ethers.getContractFactory('MonoswapToken');
+    })
+    
+    beforeEach(async function () {
+        this.weth = await this.MockERC20.deploy('WETH', 'WETH', e26);
+        this.yfi = await this.MockERC20.deploy('YFI', 'YFI', e26);
+        this.dai = await this.MockERC20.deploy('Dai', 'DAI', e26);
+        this.vusd = await this.vUSD.deploy();
 
-        await this.weth.transfer(alice, bigNum(10000000), {from: minter})
-        await this.yfi.transfer(alice, bigNum(10000000), {from: minter})
-        await this.dai.transfer(alice, bigNum(10000000), {from: minter})
+        await this.weth.transfer(this.alice.address, bigNum(10000000))
+        await this.yfi.transfer(this.alice.address, bigNum(10000000))
+        await this.dai.transfer(this.alice.address, bigNum(10000000))
 
-        await this.weth.transfer(bob, bigNum(10000000), {from: minter})
-        await this.yfi.transfer(bob, bigNum(10000000), {from: minter})
-        await this.dai.transfer(bob, bigNum(10000000), {from: minter})
-        this.monoswapToken = await MonoswapToken.new({from: minter})
-        this.pool = await Monoswap.new(this.monoswapToken.address, this.vusd.address, {from: minter})
-        this.vusd.transferOwnership(this.pool.address, {from: minter})
-        this.monoswapToken.transferOwnership(this.pool.address, {from: minter})
-        this.pool.setFeeTo(dev, {from: minter})
+        await this.weth.transfer( this.bob.address, bigNum(10000000))
+        await this.yfi.transfer( this.bob.address, bigNum(10000000))
+        await this.dai.transfer( this.bob.address, bigNum(10000000))
+        this.monoswapToken = await this.MonoswapToken.deploy()
+        this.pool = await this.Monoswap.deploy(this.monoswapToken.address, this.vusd.address)
+        // this.pool = await deployProxy(Monoswap, [this.monoswapToken.address, this.vusd.address])
+        this.vusd.transferOwnership(this.pool.address)
+        this.monoswapToken.transferOwnership(this.pool.address)
+        this.pool.setFeeTo(this.dev.address)
 
         const timestamp = (await time.latest()) + 10000;
 
-        await this.weth.approve(this.pool.address, e26, {from: alice});
-        await this.yfi.approve(this.pool.address, e26, {from: alice});
-        await this.dai.approve(this.pool.address, e26, {from: alice});
-        await this.vusd.approve(this.pool.address, e26, {from: alice});
+        await this.weth.connect(this.alice).approve(this.pool.address, e26);
+        await this.yfi.connect(this.alice).approve(this.pool.address, e26);
+        await this.dai.connect(this.alice).approve(this.pool.address, e26);
+        await this.vusd.connect(this.alice).approve(this.pool.address, e26);
 
-        await this.weth.approve(this.pool.address, e26, {from: bob});
-        await this.yfi.approve(this.pool.address, e26, {from: bob});
-        await this.dai.approve(this.pool.address, e26, {from: bob});
-        await this.vusd.approve(this.pool.address, e26, {from: bob});
+        await this.weth.connect(this.bob).approve(this.pool.address, e26);
+        await this.yfi.connect(this.bob).approve(this.pool.address, e26);
+        await this.dai.connect(this.bob).approve(this.pool.address, e26);
+        await this.vusd.connect(this.bob).approve(this.pool.address, e26);
 
-        await this.pool.addOfficialToken(this.weth.address, bigNum(300), {from: minter})
-        await this.pool.addOfficialToken(this.dai.address, bigNum(1), {from: minter})
+        await this.pool.addOfficialToken(this.weth.address, bigNum(300))
+        await this.pool.addOfficialToken(this.dai.address, bigNum(1))
 
-        await this.pool.addLiquidity(this.weth.address, 
-            bigNum(1000000), alice, {from: alice});
-        await this.pool.addLiquidity(this.dai.address, 
-            bigNum(1000000), alice, {from: alice});
+        await this.pool.connect(this.alice).addLiquidity(this.weth.address, 
+            bigNum(1000000), this.alice.address);
+        await this.pool.connect(this.alice).addLiquidity(this.dai.address, 
+            bigNum(1000000), this.alice.address);
     })
 
 
-    it('should add liquidity successfully', async () => {
+    it('should add liquidity successfully', async function () {
 
         let ethPool = await this.pool.pools(this.weth.address);
-        assert.equal(ethPool.price.toString(), bigNum(300))
+        expect(await ethPool.price.toString()).to.equal(bigNum(300))
 
-        await this.pool.addLiquidity(this.weth.address, 
-            bigNum(1000000), bob, {from: bob});
+        await this.pool.connect(this.bob).addLiquidity(this.weth.address, 
+            bigNum(1000000),  this.bob.address);
 
         ethPool = await this.pool.pools(this.weth.address);
-        assert.equal(ethPool.price.toString(), bigNum(300))
+        expect(await ethPool.price.toString()).to.equal(bigNum(300))
         
     });
 
-    it('should purchase and sell ERC-20 successfully', async () => {
+    // it('should purchase and sell ERC-20 successfully', async () => {
 
-        const deadline = (await time.latest()) + 10000
+    //     const deadline = (await time.latest()) + 10000
 
-        await this.pool.swapExactTokenForToken(
-            this.weth.address, this.dai.address, 
-            bigNum(2), bigNum(400), bob, deadline, {from: bob})
+    //     await this.pool.swapExactTokenForToken(
+    //         this.weth.address, this.dai.address, 
+    //         bigNum(2), bigNum(400),  this.bob, deadline, {from:  this.bob})
 
-        const daiAmount = await this.dai.balanceOf(bob)
+    //     const daiAmount = await this.dai.balanceOf( this.bob)
 
-        const ethPool = await this.pool.pools(this.weth.address);
+    //     const ethPool = await this.pool.pools(this.weth.address);
 
-        const daiPool = await this.pool.pools(this.dai.address);
+    //     const daiPool = await this.pool.pools(this.dai.address);
 
-        assert.isBelow(smallNum(daiAmount.toString())-10000000, 600)
-        assert.isAbove(smallNum(daiAmount.toString())-10000000, 550)
+    //     assert.isBelow(smallNum(daiAmount.toString())-10000000, 600)
+    //     assert.isAbove(smallNum(daiAmount.toString())-10000000, 550)
 
-        assert.isAbove(smallNum(daiPool.price.toString()), 1)
-        assert.isBelow(smallNum(daiPool.price.toString()), 2)
+    //     assert.isAbove(smallNum(daiPool.price.toString()), 1)
+    //     assert.isBelow(smallNum(daiPool.price.toString()), 2)
 
-        assert.isAbove(smallNum(ethPool.price.toString()), 200)
-        assert.isBelow(smallNum(ethPool.price.toString()), 300)
+    //     assert.isAbove(smallNum(ethPool.price.toString()), 200)
+    //     assert.isBelow(smallNum(ethPool.price.toString()), 300)
         
-    });
+    // });
 
-    it('should purchase and sell vUSD successfully', async () => {
+    // it('should purchase and sell vUSD successfully', async () => {
 
-        const deadline = (await time.latest()) + 10000
+    //     const deadline = (await time.latest()) + 10000
 
-        await this.pool.swapExactTokenForToken(
-            this.weth.address, this.vusd.address, 
-            bigNum(20), bigNum(4000), bob, deadline,
-            {from: bob})
+    //     await this.pool.swapExactTokenForToken(
+    //         this.weth.address, this.vusd.address, 
+    //         bigNum(20), bigNum(4000),  this.bob, deadline,
+    //         {from:  this.bob})
 
-        let vusdBob0 = smallNum((await this.vusd.balanceOf(bob)).toString())
+    //     let vusdbob0 = smallNum((await this.vusd.balanceOf( this.bob)).toString())
 
-        assert.isBelow(vusdBob0, 6000)
-        assert.isAbove(vusdBob0, 5500)
+    //     assert.isBelow(vusdbob0, 6000)
+    //     assert.isAbove(vusdbob0, 5500)
 
-        let ethPool = await this.pool.pools(this.weth.address)
-        let ethPrice0 = smallNum(ethPool.price.toString())
+    //     let ethPool = await this.pool.pools(this.weth.address)
+    //     let ethPrice0 = smallNum(ethPool.price.toString())
 
-        assert.isAbove(ethPrice0, 200)
-        assert.isBelow(ethPrice0, 300)
+    //     assert.isAbove(ethPrice0, 200)
+    //     assert.isBelow(ethPrice0, 300)
 
-        console.log('ETH price', ethPrice0)
+    //     console.log('ETH price', ethPrice0)
 
-        await this.pool.swapTokenForExactToken(
-            this.vusd.address, this.weth.address, 
-            bigNum(3500), bigNum(10), bob, deadline,
-            {from: bob})
+    //     await this.pool.swapTokenForExactToken(
+    //         this.vusd.address, this.weth.address, 
+    //         bigNum(3500), bigNum(10),  this.bob, deadline,
+    //         {from:  this.bob})
 
-        let vusdBob1 = smallNum((await this.vusd.balanceOf(bob)).toString())
+    //     let vusdbob1 = smallNum((await this.vusd.balanceOf( this.bob)).toString())
 
-        assert.isBelow(vusdBob0-vusdBob1, 3020)
-        assert.isAbove(vusdBob0-vusdBob1, 3000)
+    //     assert.isBelow(vusdbob0-vusdbob1, 3020)
+    //     assert.isAbove(vusdbob0-vusdbob1, 3000)
 
-        ethPool = await this.pool.pools(this.weth.address)
-        const ethPrice1 = smallNum(ethPool.price.toString())
-        console.log('ETH price', ethPrice1)
-        assert.isBelow(ethPrice0, ethPrice1)
+    //     ethPool = await this.pool.pools(this.weth.address)
+    //     const ethPrice1 = smallNum(ethPool.price.toString())
+    //     console.log('ETH price', ethPrice1)
+    //     assert.isBelow(ethPrice0, ethPrice1)
         
-    });
+    // });
 
-    it('should remove liquidity successfully', async () => {
+    // it('should remove liquidity successfully', async () => {
 
-        const deadline = (await time.latest()) + 10000
+    //     const deadline = (await time.latest()) + 10000
 
-        await this.pool.swapExactTokenForToken(
-            this.dai.address, this.weth.address, 
-            bigNum(15000), bigNum(45), bob, deadline,
-            {from: bob})
-        const liquidity = (await this.monoswapToken.balanceOf(alice, 0)).toString()
+    //     await this.pool.swapExactTokenForToken(
+    //         this.dai.address, this.weth.address, 
+    //         bigNum(15000), bigNum(45),  this.bob, deadline,
+    //         {from:  this.bob})
+    //     const liquidity = (await this.monoswapToken.balanceOf(this.alice, 0)).toString()
 
-        console.log('liquidity', liquidity);
+    //     console.log('liquidity', liquidity);
 
-        const results = await this.pool.removeLiquidity(
-            this.weth.address, liquidity, alice, 0, 0, {from: alice});
+    //     const results = await this.pool.removeLiquidity(
+    //         this.weth.address, liquidity, this.this.alice, 0, 0, {from: this.this.alice});
 
-        let vusdAmount = await this.vusd.balanceOf(alice)
+    //     let vusdAmount = await this.vusd.balanceOf(this.alice)
 
-        assert.isBelow(smallNum(vusdAmount.toString()), 50*300)
-        assert.isAbove(smallNum(vusdAmount.toString()), 50*250)
+    //     assert.isBelow(smallNum(vusdAmount.toString()), 50*300)
+    //     assert.isAbove(smallNum(vusdAmount.toString()), 50*250)
 
-        let devFee = await this.vusd.balanceOf(dev)
-        console.log(smallNum(devFee.toString()))
+    //     let devFee = await this.vusd.balanceOf(this.dev)
+    //     console.log(smallNum(devFee.toString()))
         
-    });
+    // });
 
-    it('should add and remove liquidity successfully', async () => {
+    // it('should add and remove liquidity successfully', async () => {
 
-        const deadline = (await time.latest()) + 10000
+    //     const deadline = (await time.latest()) + 10000
 
-        await this.pool.swapExactTokenForToken(
-            this.dai.address, this.weth.address, 
-            bigNum(15000), bigNum(45), bob, deadline,
-            {from: bob})
+    //     await this.pool.swapExactTokenForToken(
+    //         this.dai.address, this.weth.address, 
+    //         bigNum(15000), bigNum(45),  this.bob, deadline,
+    //         {from:  this.bob})
 
-        await this.pool.addLiquidity(this.weth.address, 
-            bigNum(1000000), bob, {from: bob});
-        const liquidity = (await this.monoswapToken.balanceOf(alice, 0)).toString()
+    //     await this.pool.addLiquidity(this.weth.address, 
+    //         bigNum(1000000),  this.bob, {from:  this.bob});
+    //     const liquidity = (await this.monoswapToken.balanceOf(this.alice, 0)).toString()
 
-        console.log('liquidity', liquidity);
+    //     console.log('liquidity', liquidity);
 
-        const results = await this.pool.removeLiquidity(
-            this.weth.address, liquidity, alice, 0, 0, {from: alice});
+    //     const results = await this.pool.removeLiquidity(
+    //         this.weth.address, liquidity, this.this.alice, 0, 0, {from: this.this.alice});
 
-        let vusdAmount = await this.vusd.balanceOf(alice)
+    //     let vusdAmount = await this.vusd.balanceOf(this.alice)
 
-        assert.isBelow(smallNum(vusdAmount.toString()), 50*300/2)
-        assert.isAbove(smallNum(vusdAmount.toString()), 50*250/2)
+    //     assert.isBelow(smallNum(vusdAmount.toString()), 50*300/2)
+    //     assert.isAbove(smallNum(vusdAmount.toString()), 50*250/2)
 
-        let devFee = await this.vusd.balanceOf(dev)
-        console.log(smallNum(devFee.toString()))
+    //     let devFee = await this.vusd.balanceOf(this.dev)
+    //     console.log(smallNum(devFee.toString()))
         
-    });
+    // });
 
-    it('should list new tokens successfully', async () => {
+    // it('should list new tokens successfully', async () => {
 
-        const deadline = (await time.latest()) + 10000
+    //     const deadline = (await time.latest()) + 10000
 
-        await this.pool.listNewToken(
-            this.yfi.address, bigNum(20000), 
-            0, bigNum(20), bob, {from: bob})
+    //     await this.pool.listNewToken(
+    //         this.yfi.address, bigNum(20000), 
+    //         0, bigNum(20),  this.bob, {from:  this.bob})
 
-        const yfiAlice0 = smallNum((await this.yfi.balanceOf(alice)).toString())
-        const daiAlice0 = smallNum((await this.dai.balanceOf(alice)).toString())
+    //     const yfiAlice0 = smallNum((await this.yfi.balanceOf(this.alice)).toString())
+    //     const daiAlice0 = smallNum((await this.dai.balanceOf(this.alice)).toString())
 
-        let yfiPool = await this.pool.pools(this.yfi.address)
-        const yfiPrice0 = smallNum(yfiPool.price.toString())
+    //     let yfiPool = await this.pool.pools(this.yfi.address)
+    //     const yfiPrice0 = smallNum(yfiPool.price.toString())
 
-        await this.pool.swapTokenForExactToken(
-            this.dai.address, this.yfi.address, 
-            bigNum(30000), bigNum(1), alice, deadline,
-            {from: alice})
+    //     await this.pool.swapTokenForExactToken(
+    //         this.dai.address, this.yfi.address, 
+    //         bigNum(30000), bigNum(1), this.this.alice, deadline,
+    //         {from: this.this.alice})
 
-        const yfiAlice1 = smallNum((await this.yfi.balanceOf(alice)).toString())
-        const daiAlice1 = smallNum((await this.dai.balanceOf(alice)).toString())
-        assert.equal(yfiAlice1-yfiAlice0, 1)
-        assert.isBelow(daiAlice0-daiAlice1, 22000)
-        assert.isAbove(daiAlice0-daiAlice1, 20000)
+    //     const yfiAlice1 = smallNum((await this.yfi.balanceOf(this.alice)).toString())
+    //     const daiAlice1 = smallNum((await this.dai.balanceOf(this.alice)).toString())
+    //     assert.equal(yfiAlice1-yfiAlice0, 1)
+    //     assert.isBelow(daiAlice0-daiAlice1, 22000)
+    //     assert.isAbove(daiAlice0-daiAlice1, 20000)
 
-        yfiPool = await this.pool.pools(this.yfi.address)
-        const yfiPrice1 = smallNum(yfiPool.price.toString())
+    //     yfiPool = await this.pool.pools(this.yfi.address)
+    //     const yfiPrice1 = smallNum(yfiPool.price.toString())
 
-        assert.isAbove(yfiPrice1, yfiPrice0)
-        console.log('yfi', yfiPrice1, yfiPrice0)
+    //     assert.isAbove(yfiPrice1, yfiPrice0)
+    //     console.log('yfi', yfiPrice1, yfiPrice0)
         
-    });
+    // });
 
 });
