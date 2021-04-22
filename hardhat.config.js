a439b51e809884309db20d479d6eab09c9ed625b
AnichoKraus <anichokraus3123@gmail.com>
2021-04-22 11:19:54 -0500

    added kovan

--- hardhat.config.js
+++ hardhat.config.js
@@ -44,6 +44,10 @@ module.exports = {
       url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
     },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
