0951243fe370a7518a196bcc206c8518acc135a8
AnichoKraus <anichokraus3123@gmail.com>
2021-04-22 17:55:09 -0400

    add WETH address

--- scripts/deploy-monoswap.js
+++ scripts/deploy-monoswap.js
@@ -1,48 +1,54 @@
-const hre = require("hardhat");
-const { ethers, upgrades } = require("hardhat");
-const Web3 = require('web3');
-const { utils } = Web3;
+const hre = require("hardhat")
+const { ethers, upgrades } = require("hardhat")
 async function main() {
   
-  const [deployer] = await ethers.getSigners();
+  const [deployer] = await ethers.getSigners()
 
   console.log(
     "Deploying contracts with the account:",
     deployer.address
-  );
+  )
   
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-  const MonoXPool = await ethers.getContractFactory("MonoXPool");
-  const Monoswap = await ethers.getContractFactory("Monoswap");
-  const WETH9 = await ethers.getContractFactory("WETH9");
-  const VUSD = await ethers.getContractFactory('VUSD');
-  const weth = await WETH9.deploy();
-  console.log("WETH address:", weth.address);
-  const vusd = await VUSD.deploy();
-  console.log("VUSD address:", vusd.address);
-  await vusd.deployed();
-  const monoXPool = await MonoXPool.deploy(weth.address);
-  console.log("MonoXPool address:", monoXPool.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString())
+  const network = (await ethers.provider.getNetwork()).name
+  const MonoXPool = await ethers.getContractFactory("MonoXPool")
+  const Monoswap = await ethers.getContractFactory("Monoswap")
+  const VUSD = await ethers.getContractFactory('VUSD')
+  let WETH
+  switch (network) {
+    case 'mainnet':
+      WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+      break
+    case 'kovan':
+      WETH = '0xd0A1E359811322d97991E03f863a0C30C2cF029C'
+      break
+    case 'ropsten':
+      WETH = '0xc778417e063141139fce010982780140aa0cd5ab'
+      break
+    case 'rinkeby':
+      WETH = '0xc778417e063141139fce010982780140aa0cd5ab'
+      break
+    default:
+      throw new Error("unknown network");
+  }
+  const vusd = await VUSD.deploy()
+  console.log("VUSD address:", vusd.address)
+  await vusd.deployed()
+  const monoXPool = await MonoXPool.deploy(WETH)
+  console.log("MonoXPool address:", monoXPool.address)
   const monoswap = await upgrades.deployProxy(Monoswap, [monoXPool.address, vusd.address])
-  console.log("Monoswap address:", monoswap.address);
-  await weth.deployed();
-  await vusd.deployed();
-  await monoXPool.deployed();
-  await monoswap.deployed();
+  console.log("Monoswap address:", monoswap.address)
+  await vusd.deployed()
+  await monoXPool.deployed()
+  await monoswap.deployed()
   
-  await vusd.transferOwnership(monoswap.address);
-  await monoXPool.transferOwnership(monoswap.address);
-  const devAddr = deployer.address;
-  await monoswap.setFeeTo(devAddr);
+  await vusd.transferOwnership(monoswap.address)
+  await monoXPool.transferOwnership(monoswap.address)
+  const devAddr = deployer.address
+  await monoswap.setFeeTo(devAddr)
   
   await hre.run("verify:verify", {
     address: vusd.address,
-    constructorArguments: [
-    ],
-  })
-
-  await hre.run("verify:verify", {
-    address: weth.address,
     constructorArguments: [
     ],
   })
@@ -53,10 +59,9 @@ async function main() {
       weth.address
     ],
   })
-  const networkName = (await ethers.provider.getNetwork()).name;
   const oz_monoswap = require("../.openzeppelin/" + networkName + ".json")
   const monoswapImplAddress = oz_monoswap.impls[Object.keys(oz_monoswap.impls)[0]].address
-  console.log('Monoswap Impl Address', monoswapImplAddress)
+  console.log("Monoswap Impl Address", monoswapImplAddress)
   await hre.run("verify:verify", {
     address: monoswapImplAddress,
     constructorArguments: [
