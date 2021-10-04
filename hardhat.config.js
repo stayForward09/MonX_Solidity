4b26b5b8d04521b1b0a21199457d745cf7d2424a
AnichoKraus <anichokraus3123@gmail.com>
2021-10-04 15:21:44 -0500

    added goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -215,6 +215,10 @@ module.exports = {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
     },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
